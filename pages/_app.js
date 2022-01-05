517b38ee706e21a610c0ab12365ca8ee7cbbea8f
Matteo Gassend <mgassend@gmail.com>
2022-01-05 10:51:38 +0100

    :bookmark: initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <div className='h-screen w-screen flex flex-col'>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by Matteo Gassend & Trakt
+          <span>
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
 }
 
 export default MyApp
